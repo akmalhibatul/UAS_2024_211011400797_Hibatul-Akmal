cbf55f54ec26845a5c95f7fddddfe5ebd33710c8
Hibatul Akmal <akmalhibatul1@gmail.com>
2024-07-04 20:03:59 +0700

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'crypto.dart';
 import 'crypto_service.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(CryptoApp());
@@ -10,13 +14,31 @@ void main() {
 class CryptoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Crypto Prices',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider<ThemeProvider>(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            title: 'Crypto Prices',
+            theme: themeProvider.getTheme(),
+            home: CryptoListScreen(),
+          );
+        },
       ),
-      home: CryptoListScreen(),
     );
+  }
+}
+
+class ThemeProvider extends ChangeNotifier {
+  bool isDarkMode = false;
+
+  ThemeData getTheme() {
+    return isDarkMode ? ThemeData.dark() : ThemeData.light();
+  }
+
+  void toggleTheme() {
+    isDarkMode = !isDarkMode;
+    notifyListeners();
   }
 }
 
@@ -28,11 +50,30 @@ class CryptoListScreen extends StatefulWidget {
 class _CryptoListScreenState extends State<CryptoListScreen> {
   late Future<List<Crypto>> futureCryptos;
   final CryptoService cryptoService = CryptoService();
+  late Timer timer;
+  String currentTime = '';
 
   @override
   void initState() {
     super.initState();
     futureCryptos = cryptoService.fetchCryptos();
+    timer =
+        Timer.periodic(Duration(seconds: 1), (Timer t) => _getCurrentTime());
+  }
+
+  void _getCurrentTime() {
+    final DateTime now = DateTime.now().toUtc().add(Duration(hours: 7));
+    final String formattedDateTime =
+        DateFormat('yyyy-MM-dd HH:mm:ss').format(now);
+    setState(() {
+      currentTime = formattedDateTime;
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 
   @override
@@ -40,13 +81,21 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Crypto Prices'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.brightness_6),
+            onPressed: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+          ),
+        ],
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
-              'Today: ${DateFormat.yMMMd().format(DateTime.now())}',
+              'Jakarta Time: $currentTime',
               style: TextStyle(fontSize: 18),
             ),
           ),
@@ -70,23 +119,46 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
                     itemCount: cryptos.length,
                     itemBuilder: (context, index) {
                       final crypto = cryptos[index];
-                      return Card(
-                        elevation: 4.0,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                crypto.name,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+                      return GestureDetector(
+                        onTap: () => _showCryptoDetails(context, crypto),
+                        child: Card(
+                          elevation: 4.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  crypto.name,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(crypto.symbol),
-                              SizedBox(height: 8.0),
-                              Text('\$${crypto.priceUsd.toStringAsFixed(2)}'),
-                            ],
+                                Text(crypto.symbol),
+                                SizedBox(height: 8.0),
+                                FutureBuilder<double>(
+                                  future: _convertToIDR(crypto.priceUsd),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return Text('Loading...');
+                                    } else if (snapshot.hasError) {
+                                      return Text('Error');
+                                    } else if (snapshot.hasData) {
+                                      final formatter = NumberFormat.currency(
+                                          locale: 'id',
+                                          symbol: 'Rp ',
+                                          decimalDigits: 0);
+                                      return Text(
+                                          formatter.format(snapshot.data));
+                                    } else {
+                                      return Text('N/A');
+                                    }
+                                  },
+                                ),
+                                Text('\$${crypto.priceUsd.toStringAsFixed(2)}'),
+                              ],
+                            ),
                           ),
                         ),
                       );
@@ -101,5 +173,60 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
         ],
       ),
     );
+  }
+
+  Future<void> _showCryptoDetails(BuildContext context, Crypto crypto) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(crypto.name),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Symbol: ${crypto.symbol}'),
+              Text('Price USD: \$${crypto.priceUsd.toStringAsFixed(2)}'),
+              FutureBuilder<double>(
+                future: _convertToIDR(crypto.priceUsd),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading...');
+                  } else if (snapshot.hasError) {
+                    return Text('Error');
+                  } else if (snapshot.hasData) {
+                    final formatter = NumberFormat.currency(
+                        locale: 'id', symbol: 'Rp ', decimalDigits: 0);
+                    return Text(
+                        'Price IDR: ${formatter.format(snapshot.data)}');
+                  } else {
+                    return Text('N/A');
+                  }
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<double> _convertToIDR(double priceUsd) async {
+    final response = await http
+        .get(Uri.parse('https://api.exchangerate-api.com/v4/latest/USD'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final double rate = data['rates']['IDR'];
+      return priceUsd * rate;
+    } else {
+      throw Exception('Failed to load exchange rate');
+    }
   }
 }
